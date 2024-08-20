e6b8033e75d76afe12e41fa57589da09ad846be6
Kamvelihle-1 <ksikalana@gmail.com>
2024-08-20 10:53:59 +0200

    updates

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
-
+const helmet      = require('helmet');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
 const app = express();
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,6 +18,11 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helmet.contentSecurityPolicy({directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'"],
+  styleSrc: ["'self'"]
+}}));
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
